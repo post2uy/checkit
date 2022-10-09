aa4eb2a9024f9b782af6fee654cbe1fa91f5180d
Y U <post2uy@gmail.com>
2022-10-09 08:00:01 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox2</h1>
+      <h1>222Hello CodeSandbox2</h1>
       <h2>2Start editing to see some magic happen!</h2>
     </div>
   );
