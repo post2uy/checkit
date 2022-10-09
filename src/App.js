9e4b094e82f936a0d6a21dd55b065114b94036b7
UY <71317493+post2uy@users.noreply.github.com>
2022-10-09 16:59:01 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox2</h1>
+      <h2>2Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
